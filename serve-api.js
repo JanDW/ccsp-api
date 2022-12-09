965f2f4cb5a7e2b34bcc5d9c772862d3f2272671
Jan De Wilde <jan@jandewilde.com>
2022-12-09 11:04:50 -0500

    Remove opn use

--- serve-api.js
+++ serve-api.js
@@ -2,7 +2,7 @@
 
 const jsonServer = require('json-server');
 const nocache = require('nocache');
-const opn = require('opn');
+// const opn = require('opn');
 const server = jsonServer.create();
 const router = jsonServer.router('api.json');
 const middlewares = jsonServer.defaults();
@@ -12,5 +12,5 @@ server.use(router);
 server.use(nocache());
 server.listen(3000, () => {
   console.log('JSON Server is running.');
-  opn('http://localhost:3000');
+  // opn('http://localhost:3000');
 });
